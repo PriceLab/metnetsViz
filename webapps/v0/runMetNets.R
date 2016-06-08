2e350c2ad8025ce44ef87bf4d4f1786500a2063c
paul-shannon <paul.thurmond.shannon@gmail.com>
2016-06-07 21:03:36 -0700

    changed port to 7002

--- webapps/v0/runMetNets.R
+++ webapps/v0/runMetNets.R
@@ -1,7 +1,7 @@
 library(ChinookServer)
 stopifnot(packageVersion("ChinookServer") >= "1.0.8")
 library(RUnit)
-PORT=7001
+PORT=7002
 datasets <- "Maripaludis"
 analysisPackages <- c("ChinookLinearModel")
 browserFile <- "index.html"
